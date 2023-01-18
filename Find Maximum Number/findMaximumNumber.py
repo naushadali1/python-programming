7c54dfaad1aab004e4f9996e3a4c92d4196ae3c9
Naushad Ali <nashadali585@gmail.com>
2023-01-18 18:34:13 +0500

    Create findMaximumNumber.py
    Find maximum numbers between two numbers

---
+++ Find Maximum Number/findMaximumNumber.py
@@ -0,0 +1,7 @@
+num1 = input("Input First Number")
+num2 = input("Input Second Number")
+
+if num1 > num2:
+    print(num1, "is greater than ", num2)
+else:
+    print(num2, "is greater than ", num1)
