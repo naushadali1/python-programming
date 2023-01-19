4db911b5b30b7e3e88aa8633155e81582b31df1c
Naushad Ali <nashadali585@gmail.com>
2023-01-20 03:27:37 +0500

    Create strings.py
    strings basic methods

---
+++ Strings/strings.py
@@ -0,0 +1,14 @@
+name = "Naushad Ali"
+fruit = 'he said , "I want to eat an Apple" '
+multiple_line = """hello
+how are  
+mr"""
+print("Hello", name)
+print(name.upper())
+print(name.lower())
+print(name.replace("Naushad", "Muhammad"))
+print(name.split(" "))
+print(fruit)
+print(len(fruit))
+print(fruit[3])
+print(multiple_line)
